bdc12bf76786781315f99123a1c8ee465a083eb5
crohi <crohitp9@gmail.com>
2023-01-21 22:46:02 +0530

    programming

---
+++ solved_problems/word_pattern.py
@@ -0,0 +1,27 @@
+def wordPattern(pattern: str, s: str) -> bool:
+    words = s.split(" ")
+
+    if len(pattern) != len(words):
+        return False
+
+    charToWord = {}
+    wordToChar = {}
+
+    for c, w in zip(pattern, words):
+
+        if c in charToWord and charToWord[c] != w:
+            return False
+
+        if w in wordToChar and wordToChar[w] != c:
+            return False
+
+        charToWord[c] = w
+        wordToChar[w] = c
+
+    return True
+
+
+p = "abba"
+# k = "dog cat cat dog"
+k = "dog cat cat fish"
+print(wordPattern(p, k))
