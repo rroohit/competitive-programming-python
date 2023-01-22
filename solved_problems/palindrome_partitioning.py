9dd456c9c85593dc1cbf3cfbc6147dc570a86a2f
crohi <crohitp9@gmail.com>
2023-01-22 19:52:21 +0530

    programming

---
+++ solved_problems/palindrome_partitioning.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+def partition(s: str) -> List[List[str]]:
+    res = []
+    part = []
+
+    def dfs(i):
+        if i >= len(s):
+            res.append(part.copy())
+            return
+
+        for j in range(i, len(s)):
+            if isPalindrome(s, i, j):
+                part.append(s[i:j + 1])
+                dfs(j + 1)
+                part.pop()
+
+    dfs(0)
+
+    return res
+
+
+def isPalindrome(s, l, r):
+    while l < r:
+        if s[l] != s[r]:
+            return False
+        l, r = l + 1, r - 1
+
+    return True
+
+
+k = "aabbaa"
+print(partition(k))
